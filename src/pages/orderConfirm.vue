3015caf9ec0e5b2ab6aae7621b568ce653422f1a
Daisy-Rou <2098646597@qq.com>
2020-06-27 22:29:23 +0800

    地址删除功能

--- src/pages/orderConfirm.vue
+++ src/pages/orderConfirm.vue
@@ -32,20 +32,7 @@
                     + item.receiverCity + '' + item.receiverDistrict +''
                     + item.receiverAddress}}<br>东大街地铁</div>
                 <div class="action">
-                  <a href="javascript:;" class="fl">
-                    <svg class="icon icon-del"><use xlink:href="#icon-del"></use></svg>
-                  </a>
-                  <a href="javascript:;" class="fr">
-                    <svg class="icon icon-edit"><use xlink:href="#icon-edit"></use></svg>
-                  </a>
-                </div>
-              </div>
-              <div class="addr-info">
-                <h2>小马哥</h2>
-                <div class="phone">176****1717</div>
-                <div class="street">北京 北京市 昌平区 回龙观<br>东大街地铁</div>
-                <div class="action">
-                  <a href="javascript:;" class="fl">
+                  <a href="javascript:;" class="fl" @click="delAddress(item)">
                     <svg class="icon icon-del"><use xlink:href="#icon-del"></use></svg>
                   </a>
                   <a href="javascript:;" class="fr">
@@ -111,44 +98,14 @@
       </div>
     </div>
     <modal
-      title="新增确认"
+      title="删除确认"
       btnType="1"
-      :showModal="showEditModal"
-      @cancel="showEditModal=false"
+      :showModal="showDelModal"
+      @cancel="showDelModal=false"
+      @submit="submitAddress"
     >
       <template v-slot:body>
-        <div class="edit-wrap">
-          <div class="item">
-            <input type="text" class="input" placeholder="姓名">
-            <input type="text" class="input" placeholder="手机号">
-          </div>
-          <div class="item">
-            <select name="province">
-              <option value="北京">北京</option>
-              <option value="天津">天津</option>
-              <option value="河北">河北</option>
-            </select>
-            <select name="city">
-              <option value="北京">北京</option>
-              <option value="天津">天津</option>
-              <option value="河北">石家庄</option>
-            </select>
-            <select name="district">
-              <option value="北京">昌平区</option>
-              <option value="天津">海淀区</option>
-              <option value="河北">东城区</option>
-              <option value="天津">西城区</option>
-              <option value="河北">顺义区</option>
-              <option value="天津">房山区</option>
-            </select>
-          </div>
-          <div class="item">
-            <textarea name="street"></textarea>
-          </div>
-          <div class="item">
-            <input type="text" class="input" placeholder="邮编">
-          </div>
-        </div>
+        <p>您确认要删除此地址吗？</p>
       </template>
     </modal>
   </div>
@@ -163,7 +120,10 @@ export default{
         cartList: [], //购物车中需要结算的商品列表
         showEditModal:false,//是否显示新增或者编辑弹框
         cartTotalPrice: 0, //商品总金额
-        count: 0 //商品结算数量
+        count: 0 ,//商品结算数量
+        checkedItem: {}, //选中的商品对象
+        userAction: '', //用户行为 0：新增 1：编辑 2：删除
+        showDelModal: false, //是否显示删除弹框
     }
   },
   components:{
@@ -178,6 +138,37 @@ export default{
         this.axios.get('/shippings').then((res) => {
             this.list = res.list
         })
+    },
+    delAddress(item) {
+        this.checkedItem = item
+        this.userAction = 2
+        this.showDelModal = true
+    },
+    //地址删除 编辑 新增 功能
+    submitAddress() {
+        let {checkedItem, userAction} = this
+        let method, url
+        if(userAction == 0) {
+            method = 'post'
+            url = '/shippings'
+        }else if(userAction == 1) {
+            method = 'put'
+            url = `/shippings/${checkedItem.id}`
+        }else {
+            method = 'delete'
+            url = `/shippings/${checkedItem.id}`
+        }
+        this.axios[method](url).then(() => {
+            this.closeModal()
+            //获取最新地址
+            this.getAddressList()
+            this.$message.success('操作成功！')
+        })
+    },
+    closeModal() {
+        this.checkedItem = {}
+        this.userAction = ''
+        this.showDelModal = false
     },
     getCartList() {
         this.axios.get('/carts').then((res) => {
