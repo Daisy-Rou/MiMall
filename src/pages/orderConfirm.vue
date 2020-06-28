7baddace1f69f2801a91a46274f8e97ed3ad1869
Daisy-Rou <2098646597@qq.com>
2020-06-28 21:24:00 +0800

    地址编辑和订单提交

--- src/pages/orderConfirm.vue
+++ src/pages/orderConfirm.vue
@@ -25,7 +25,8 @@
           <div class="item-address">
             <h2 class="addr-title">收货地址</h2>
             <div class="addr-list clearfix">
-              <div class="addr-info" v-for="(item, index) in list" :key="index">
+              <div class="addr-info" :class="{'checked':index == checkIndex}"
+              @click="checkIndex = index" v-for="(item, index) in list" :key="index">
                 <h2>{{item.receiverName}}</h2>
                 <div class="phone">{{item.receiverMobile}}</div>
                 <div class="street">{{item.receiverProvince + '' 
@@ -35,7 +36,7 @@
                   <a href="javascript:;" class="fl" @click="delAddress(item)">
                     <svg class="icon icon-del"><use xlink:href="#icon-del"></use></svg>
                   </a>
-                  <a href="javascript:;" class="fr">
+                  <a href="javascript:;" class="fr" @click="editAddressModal(item)">
                     <svg class="icon icon-edit"><use xlink:href="#icon-edit"></use></svg>
                   </a>
                 </div>
@@ -92,7 +93,7 @@
           </div>
           <div class="btn-group">
             <a href="/#/cart" class="btn btn-default btn-large">返回购物车</a>
-            <a href="javascript:;" class="btn btn-large">去结算</a>
+            <a href="javascript:;" class="btn btn-large" @click="orderSubmit">去结算</a>
           </div>
         </div>
       </div>
@@ -166,6 +167,7 @@ export default{
         checkedItem: {}, //选中的商品对象
         userAction: '', //用户行为 0：新增 1：编辑 2：删除
         showDelModal: false, //是否显示删除弹框
+        checkIndex: 0 //当前收货地址选中的索引
     }
   },
   components:{
@@ -187,6 +189,13 @@ export default{
         this.checkedItem = {}
         this.showEditModal = true
     },
+    //打开编辑地址弹框
+    editAddressModal(item) {
+        this.userAction = 1
+        this.checkedItem = item
+        this.showEditModal = true
+    },
+    //打开删除地址弹框
     delAddress(item) {
         this.checkedItem = item
         this.userAction = 2
@@ -259,6 +268,24 @@ export default{
                 this.count += item.quantity
             })
         })  
+    },
+    //订单提交
+    orderSubmit() {
+        let item = this.list[this.checkIndex]
+        if(!item) {
+            this.$message.error('请选择一个收货地址！')
+            return
+        }
+        this.axios.post('/orders', {
+            shippingId: item.id
+        }).then((res) => {
+            this.$router.push({
+                path: '/order/pay',
+                query: {
+                    orderNo:res.orderNo   
+                }
+            })
+        })
     }
   }
 }
