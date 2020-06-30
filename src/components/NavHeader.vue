1b28adbaf07a28fed702e18815cd3e52bc9b8f1f
Daisy-Rou <2098646597@qq.com>
2020-06-30 11:18:55 +0800

    Loading和NoData优化

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -13,7 +13,7 @@
                     <a href="javascript:;" v-if="username">{{username}}</a>
                     <a href="javascript:;" v-if="!username" @click="login">登录</a>
                     <a href="javascript:;" v-if="username" @click="logout">注销</a>
-                    <a href="javascript:;" v-if="username">我的订单</a>
+                    <a href="/#/order/list" v-if="username" >我的订单</a>
                     <!-- v-on 事件绑定 也可以简写 @click -->
                     <a href="javascript:;" class="my-cart" @click="goToCart">
                         <span class="icon-cart"></span>
